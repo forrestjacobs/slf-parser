2014f7d33e4e6df809da389ec5074ba1abc38c32
Forrest Jacobs <forrestjacobs@gmail.com>
2018-07-17 22:56:51 -0400

    Update Jest to use `.toMatchObject`

--- test/test.ts
+++ test/test.ts
@@ -3,36 +3,16 @@ import { safeLoadAll } from "js-yaml";
 import { toBoard } from "../dist/bundle.js";
 
 const FIXTURE_PATH = "./test";
+const FIXTURES = readdirSync(FIXTURE_PATH).filter((filename) => filename.endsWith(".yaml"));
 
-function testDocument({diagram, error, board}: any) {
-  try {
-    const result = toBoard(diagram);
+describe.each(FIXTURES)("%s", (filename) => {
+  const yaml = readFileSync(`${FIXTURE_PATH}/${filename}`, "utf8");
+  const documents = safeLoadAll(yaml).map((document) => [document.name, document]);
+  it.each(documents)("%s", (name, {diagram, error, board}) => {
     if (error) {
-      fail("Expected error, but it did not occur");
+      return expect(() => toBoard(diagram)).toThrow();
     }
-    for (const key of Object.keys(board)) {
-      expect(result[key]).toEqual(board[key]);
-    }
-  } catch (exception) {
-    if (error) {
-      return;
-    }
-    throw new Error(`${exception.message}\nDiagram:\n${diagram}\nLocation: ${JSON.stringify(exception.location)}`);
-  }
-}
 
-function testFixture(filename: string) {
-  for (const document of safeLoadAll(readFileSync(`${FIXTURE_PATH}/${filename}`, "utf8"))) {
-    it(document.name, () => testDocument(document));
-  }
-}
-
-function testFixtures() {
-  for (const filename of readdirSync(FIXTURE_PATH)) {
-    if (filename.endsWith(".yaml")) {
-      describe(filename, () => testFixture(filename));
-    }
-  }
-}
-
-testFixtures();
+    expect(toBoard(diagram)).toMatchObject(board);
+  });
+});
