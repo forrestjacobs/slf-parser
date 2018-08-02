5ac242f4649db8e1481bcf9019f5da0cc7fbf37d
Forrest Jacobs <forrestjacobs@gmail.com>
2018-08-01 20:53:55 -0400

    Export the order of column labels

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Board } from "./board";
 import { generateBoard } from "./generate-board";
 import { parse } from "./parse.pegjs";
 
+export { COLUMN_ALPHA } from "./board";
+
 export function toBoard(slf: string): Board {
   const parseTree = parse(slf);
   return generateBoard(parseTree);
