ad516e7080ee6c9452c08a60af120d519df3e198
Forrest Jacobs <forrestjacobs@gmail.com>
2018-08-04 16:34:54 -0400

    Remove dead code from pegjs’s generated parser

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,21 @@ import typescript from "typescript";
 function pegjs(options) {
   return {
     transform(grammar, id) {
-      return id.endsWith(".pegjs") ? generate(grammar, { format: "es", output: "source", ...options }) : null;
+      if (!id.endsWith(".pegjs")) {
+        return null;
+      }
+      // Generate the parser and remove branches that aren't called
+      // This is really messy
+      return generate(grammar, { format: "es", output: "source", ...options })
+        .replace('if (typeof Error.captureStackTrace === "function")', "if (false)")
+        .replace("function peg$parse(input, options)", "function peg$parse(input)")
+        .replace("options = options !== undefined ? options : {}", "var options = {}")
+        .replace('if ("startRule" in options)', "if (false)")
+        .replace("if (invert)", "if (true)")
+        .replace('+ (expectation.inverted ? "^" : "") +', '+')
+        .replace(/other:[^,]+,/, '')
+        .replace(/(function classEscape.*)\.replace\(\/\[\\x00-\\x0F\]\/g, *function\(ch\) { return "\\\\x0" \+ hex\(ch\); }\)/s, "$1")
+        .replace(/(function classEscape.*)\.replace\(\/\[\\x10-\\x1F\\x7F-\\x9F\]\/g, function\(ch\) { return "\\\\x"  \+ hex\(ch\); }\)/s, "$1");
     },
   };
 }
