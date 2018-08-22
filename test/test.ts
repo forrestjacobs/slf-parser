442ccb08f729e0090d350ba007fc2e3286231d87
Forrest Jacobs <forrestjacobs@gmail.com>
2018-08-21 23:30:00 -0400

    Test the cjs bundle as well

--- test/test.ts
+++ test/test.ts
@@ -1,22 +1,26 @@
 import { readdirSync, readFileSync } from "fs";
 import { safeLoadAll } from "js-yaml";
-import { toBoard } from "../dist/test-bundle.js";
+import { toBoard as testToBoard } from "../dist/bundle-cjs.js";
+import { toBoard as cjsToBoard } from "../dist/test-bundle.js";
 
 const FIXTURE_PATH = "./test";
 const FIXTURES = readdirSync(FIXTURE_PATH).filter((filename) => filename.endsWith(".yaml"));
+const TO_BOARD_FNS = [testToBoard, cjsToBoard];
 
 describe.each(FIXTURES)("%s", (filename) => {
   const yaml = readFileSync(`${FIXTURE_PATH}/${filename}`, "utf8");
   const documents = safeLoadAll(yaml).map((document) => [document.name, document]);
-  it.each(documents)("%s", (name, {diagram, lacks, match, error, board}) => {
-    if (error) {
-      expect(() => toBoard(diagram)).toThrow();
-    } else if (lacks) {
-      expect(toBoard(diagram)).not.toHaveProperty(lacks);
-    } else if (match === "exact") {
-      expect(toBoard(diagram)).toStrictEqual(board);
-    } else {
-      expect(toBoard(diagram)).toMatchObject(board);
-    }
-  });
+  for (const toBoard of TO_BOARD_FNS) {
+    it.each(documents)("%s", (name, {diagram, lacks, match, error, board}) => {
+      if (error) {
+        expect(() => toBoard(diagram)).toThrow();
+      } else if (lacks) {
+        expect(toBoard(diagram)).not.toHaveProperty(lacks);
+      } else if (match === "exact") {
+        expect(toBoard(diagram)).toStrictEqual(board);
+      } else {
+        expect(toBoard(diagram)).toMatchObject(board);
+      }
+    });
+  }
 });
